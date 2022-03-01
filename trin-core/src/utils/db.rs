246a238ecbd2920e5094851eacae542a9bf3ad4e
hans <hmrtn@pm.me>
2022-03-01 13:39:42 -0800

    update: cargo fmt --all

--- trin-core/src/utils/db.rs
+++ trin-core/src/utils/db.rs
@@ -1,11 +1,11 @@
 use std::{env, fs};
 
+use crate::cli::TrinConfig;
 use directories::ProjectDirs;
 use discv5::enr::NodeId;
 use rocksdb::{Options, DB};
 use std::path::PathBuf;
 use std::time::SystemTime;
-use crate::cli::TrinConfig;
 
 const TRIN_DATA_ENV_VAR: &str = "TRIN_DATA_PATH";
 
@@ -14,12 +14,12 @@ pub fn get_data_dir(node_id: NodeId) -> String {
     let trin_config = TrinConfig::from_cli();
 
     fs::create_dir_all(&application_path).expect("Unable to create data directory folder");
-    
+
     // Append first 8 characters of Node ID
     let node_id_string = hex::encode(node_id.raw());
     let suffix = &node_id_string[..8];
     let path = format!("{}/Trin_{}", &application_path, &suffix);
-    
+
     if trin_config.ephemeral {
         fs::create_dir_all(&path).expect("Unable to create ephemeral data directory");
         let paths = fs::read_dir(&application_path).unwrap();
@@ -41,7 +41,7 @@ pub fn get_data_dir(node_id: NodeId) -> String {
                 }
             }
         }
-        // Remove the oldest last-modified Trin_nodeId directory 
+        // Remove the oldest last-modified Trin_nodeId directory
         // TODO: Twelve is arbitrary and should probably be a user preference
         if n_paths > 12 {
             fs::remove_dir_all(&min_path)
@@ -53,8 +53,8 @@ pub fn get_data_dir(node_id: NodeId) -> String {
 }
 
 pub fn get_default_data_dir() -> String {
-    //  Linux:	    $XDG_DATA_HOME/Trin or $HOME/.local/share/Trin	
-    //  macOS:	    $HOME/Library/Application Support/Trin	
+    //  Linux:	    $XDG_DATA_HOME/Trin or $HOME/.local/share/Trin
+    //  macOS:	    $HOME/Library/Application Support/Trin
     //  Windows:    C:\Users\Username\AppData\Roaming\Trin\data
     let application_path = "Trin".to_owned();
 
