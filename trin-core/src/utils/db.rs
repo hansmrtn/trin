e1a903c90561eaef19079b4ab60c43f05275709e
hans <hmrtn@pm.me>
2022-03-11 15:58:52 -0800

    updated: emphmeral flag implments tempdir
    This updates the storage location to the OS's temporary directory.

--- trin-core/src/utils/db.rs
+++ trin-core/src/utils/db.rs
@@ -1,57 +1,42 @@
 use std::{env, fs};
 
-use log::debug;
-
 use crate::cli::TrinConfig;
 use directories::ProjectDirs;
 use discv5::enr::NodeId;
 use rocksdb::{Options, DB};
 use std::path::PathBuf;
-use std::time::SystemTime;
+use tempdir::TempDir;
 
 const TRIN_DATA_ENV_VAR: &str = "TRIN_DATA_PATH";
 
+pub fn setup_temp_data_dir() {
+    // NOTE: A TRIN_DATA_PATH will overwrite --ephemeral usage
+    match env::var(TRIN_DATA_ENV_VAR) {
+        Ok(_) => {}
+        _ => {
+            let temp_dir = TempDir::new("Trin").expect("Failed to create ephmeral data directory");
+            env::set_var(TRIN_DATA_ENV_VAR, temp_dir.path());
+        }
+    }
+}
+
 pub fn get_data_dir(node_id: NodeId) -> String {
-    let application_path = env::var(TRIN_DATA_ENV_VAR).unwrap_or_else(|_| get_default_data_dir());
     let trin_config = TrinConfig::from_cli();
+
+    if trin_config.ephemeral {
+        setup_temp_data_dir();
+    }
+
+    let application_path = env::var(TRIN_DATA_ENV_VAR).unwrap_or_else(|_| get_default_data_dir());
 
     fs::create_dir_all(&application_path).expect("Unable to create data directory folder");
 
-    // Append first 8 characters of Node ID
+    // Append first 8 characters of Node ID to session path
     let node_id_string = hex::encode(node_id.raw());
     let suffix = &node_id_string[..8];
-    let session_path = format!("{}/Trin_{}", &application_path, &suffix);
+    let session_path = PathBuf::from(&application_path).join(format!("Trin_{}", &suffix));
 
-    if trin_config.ephemeral {
-        fs::create_dir_all(&session_path).expect("Unable to create ephemeral data directory");
-        let trin_paths = fs::read_dir(&application_path).unwrap();
-        // NOTE: SystemTime is machine dependent
-        let mut min_timestamp = SystemTime::now();
-        let mut min_path = PathBuf::new();
-        let mut path_count = 0;
-        for path in trin_paths {
-            path_count += 1;
-            if let Ok(path) = path {
-                // Get oldest last-modified Trin_nodeId directory timestamp and path
-                if let Ok(metadata) = path.metadata() {
-                    if metadata.modified().unwrap() < min_timestamp {
-                        min_timestamp = metadata.modified().unwrap();
-                        min_path = path.path();
-                    }
-                } else {
-                    debug!("Couldn't get metadata for {:?}", path.path());
-                }
-            }
-        }
-        // Remove the oldest last-modified Trin_nodeId directory
-        // TODO: Twelve is arbitrary and should probably be a user preference
-        if path_count > 12 {
-            fs::remove_dir_all(&min_path)
-                .expect("Failed to remove oldest modified ephemeral data directory");
-        }
-    }
-
-    session_path
+    session_path.into_os_string().into_string().unwrap()
 }
 
 pub fn get_default_data_dir() -> String {
